d50c6e4adb01c567b09a524112b902a99793c8ce
Oskii <30426408+Oskii@users.noreply.github.com>
2018-05-22 18:55:02 +0930

    Update chainparams.cpp
    update conflicting port

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -120,7 +120,7 @@ public:
         pchMessageStart[1] = 0xbd;
         pchMessageStart[2] = 0xbe;
         pchMessageStart[3] = 0xd8;
-        nDefaultPort = 22719;
+        nDefaultPort = 22711;
         nPruneAfterHeight = 100000;
         
         genesis = CreateGenesisBlock(GENESIS_TIME, 2318408309, GENESIS_DIFFICULTY, 1, 100 * COIN);
